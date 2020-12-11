b66a8a7b82643026ba96cec0f1d8def2e31fbbb7
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-12-11 12:18:15 +0100

    IMPORT VUE JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,36 @@
-console.log('ok');
+// !INCLUSIONE VUE
+import Vue from 'vue';
+
+
+const painter = new Vue ({
+    el: '#painterApp',
+
+    data: {
+        logoLinksItems: [
+        {
+            link: 'Products',
+        },
+        {
+            link: 'Brushes',
+        },
+        {
+            link: 'Wacom Tablet',
+        },
+        {
+            link: 'Special Offers',
+        },
+        {   
+            link: 'Trials',
+        },
+        {
+            link: 'Business'
+        },
+        {
+            link: 'Support'
+        },
+        {
+            link: 'Learning'
+        }
+    ],
+    }
+})
